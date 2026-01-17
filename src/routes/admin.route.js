63d71374b77117c5e97abf0fec65ab3cb2d5b3f7
Limitlessnotions <silasmomoh2017@gmail.com>
2026-01-17 22:19:57 +0100

    Update admin.route.js

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const {
+  getPendingVouchers,
   approveVoucher,
   rejectVoucher,
   blockUser,
-  unblockUser
+  unblockUser,
+  fulfillVoucher
 } = require("../controllers/adminController");
 
+router.get("/pending-vouchers", getPendingVouchers);
 router.post("/approve-voucher", approveVoucher);
 router.post("/reject-voucher", rejectVoucher);
 router.post("/block-user", blockUser);
 router.post("/unblock-user", unblockUser);
+router.post("/fulfill-voucher", fulfillVoucher);
 
 module.exports = router;
