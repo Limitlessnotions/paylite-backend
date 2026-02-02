472872f7c72677a59c4624a40e5561f8dc8c69ed
Limitlessnotions <silasmomoh2017@gmail.com>
2026-02-02 16:26:56 +0100

    Update admin.route.js

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { requireAdmin } = require("../middleware/adminJwt");
 
 const {
   getScreenings,
   screeningDecision
 } = require("../controllers/adminController");
 
-// 🔐 Protect all admin routes
-router.use(requireAdmin);
-
-// ===== Screening (M3) =====
+// ===============================
+// ADMIN ROUTES
+// ===============================
 router.get("/screenings", getScreenings);
 router.post("/screening-decision", screeningDecision);
 
