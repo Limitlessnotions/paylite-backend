e8e00f790be86f439c83caa5b908af2f0b155f7a
Limitlessnotions <silasmomoh2017@gmail.com>
2026-02-02 17:36:06 +0100

    Create agreementService.js

---
+++ src/services/agreementService.js
@@ -0,0 +1,11 @@
+async function sendLoanAgreement(user) {
+  // Placeholder
+  // Options:
+  // - WhatsApp PDF
+  // - Email attachment
+  // - Signed URL
+
+  console.log("Sending loan agreement to:", user.phone);
+}
+
+module.exports = { sendLoanAgreement };
